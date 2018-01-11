48e1884e11f6bba4811fa0af09ce8d31e4103a64
fikrirj <fikri.rahmat48@gmail.com>
2018-01-11 15:51:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/userlogin/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.userlogin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final android.support.v7.appcompat.R.attr R = ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+
+
+    }
+
+}
